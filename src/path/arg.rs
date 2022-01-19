2b3370e0d837ea2d9165e8b264ef609da900c336
Dan Gohman <dev@sunfishcode.online>
2022-01-19 15:59:05 -0800

    Use `ZString::to_string_lossy`.
    In the std branch, this method is on `ZStr` instead of `ZString` because
    it requires alloc and `ZStr` is in core.

--- src/path/arg.rs
+++ src/path/arg.rs
@@ -466,7 +466,7 @@ impl Arg for &ZStr {
 
     #[inline]
     fn to_string_lossy(&self) -> Cow<'_, str> {
-        ZStr::to_string_lossy(self)
+        ZString::to_string_lossy(self)
     }
 
     #[inline]
@@ -534,7 +534,7 @@ impl Arg for ZString {
 
     #[inline]
     fn to_string_lossy(&self) -> Cow<'_, str> {
-        ZStr::to_string_lossy(self)
+        ZString::to_string_lossy(self)
     }
 
     #[inline]
@@ -654,7 +654,7 @@ impl<'a> Arg for Cow<'a, ZStr> {
     #[inline]
     fn to_string_lossy(&self) -> Cow<'_, str> {
         let borrow: &ZStr = core::borrow::Borrow::borrow(self);
-        borrow.to_string_lossy()
+        ZString::to_string_lossy(borrow)
     }
 
     #[inline]
